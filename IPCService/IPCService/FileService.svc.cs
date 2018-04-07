07fe1726896d95d4bbe219e7e86fe167b54c14df
Alberto Molero <alberto@lab15.io>
2018-04-07 11:19:17 +0200

    Added IPCService

---
+++ IPCService/IPCService/FileService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+
+namespace IPCService
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall, ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class FileService : IFileService
+    {
+        public void SaveFile(SaveFileRequest saveFileRequest)
+        {
+            // Do your stuff
+        }
+
+        public DownloadFileResponse DownloadFile()
+        {
+            // Do your stuff
+            return null;
+        }
+    }
+}
---
+++ IPCService/IPCService/IFileService.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.ServiceModel;
+
+namespace IPCService
+{
+    [ServiceContract]
+    public interface IFileService
+    {
+        [OperationContract]
+        void SaveFile(SaveFileRequest saveFileRequest);
+
+        [OperationContract]
+        DownloadFileResponse DownloadFile();    
+    }
+
+    [MessageContract]
+    public class DownloadFileResponse
+    {
+        [MessageHeader]
+        public string FileName { get; set; }
+
+        [MessageBodyMember]
+        public Stream Stream { get; set; }
+    }
+
+    [MessageContract]
+    public class SaveFileRequest
+    {
+        [MessageHeader]
+        public string FileName { get; set; }
+
+        [MessageBodyMember]
+        public Stream Stream { get; set; }
+
+    }
+}

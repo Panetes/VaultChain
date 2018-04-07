0935b32bb5bacc74664837dc8d39627e825de0b6
Junior Carvalho <junior@lab15.io>
2018-04-07 14:45:22 +0200

    IPFS Hidden Daemon

--- TrusChain/TrustChain.Storage/IpfsWrapper.cs
+++ TrusChain/TrustChain.Storage/IpfsWrapper.cs
@@ -13,14 +13,20 @@ namespace TrusChain.Storage
 {
     public class IpfsWrapper : IDisposable
     {
-
-        private IpfsClient _client;
         private Process _process;
         private string _path;
 
         public IpfsWrapper()
         {
-            _path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            try
+            {
+                _path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            }
+            catch (Exception)
+            {
+                _path = @"D:\";
+            }
+            
         }
 
         public void Init()
@@ -61,32 +67,14 @@ namespace TrusChain.Storage
                 process.Kill();
             }
 
-            //_process = new Process
-            //{
-            //    StartInfo = new ProcessStartInfo
-            //    {
-            //        FileName = string.Format("{0}\\ipfs_daemon.exe", _path),
-            //        Arguments = "daemon",
-            //        UseShellExecute = false,
-            //        RedirectStandardOutput = true,
-            //        CreateNoWindow = true
-            //    }
-            //};
-
             _process = new Process();
             _process.StartInfo.FileName = string.Format("{0}\\ipfs_daemon.exe", _path);
             _process.StartInfo.Arguments = "daemon";
-            _process.StartInfo.CreateNoWindow= false;
-            _process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            _process.StartInfo.CreateNoWindow= true;
+            _process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             _process.Start();
-            //_process.WaitForInputIdle();
-            //while (!_process.StandardOutput.EndOfStream)
-            //{
-            //    string line = _process.StandardOutput.ReadLine();
-            //    // do something with line
-            //}
 
-    
+            Thread.Sleep(3000);
 
             Console.WriteLine("IPFS Daemon Started");
         }
@@ -96,11 +84,7 @@ namespace TrusChain.Storage
             using (var httpClient = new HttpClient() { Timeout = Timeout.InfiniteTimeSpan })
             using (var ipfs = new IpfsClient(new Uri("http://127.0.0.1:5001"), httpClient))
             {
-                //Name of the file to add
-
-                //Wrap our stream in an IpfsStream, so it has a file name.
                 IpfsStream inputStream = new IpfsStream(fileName, File.OpenRead(fileName));
-
                 try
                 {
                     MerkleNode node = await ipfs.Add(inputStream);
@@ -110,9 +94,6 @@ namespace TrusChain.Storage
                 {
                     throw ex;
                 }
-                
-
-               
             }
         }
 
@@ -121,21 +102,19 @@ namespace TrusChain.Storage
             using (var httpClient = new HttpClient() { Timeout = Timeout.InfiniteTimeSpan })
             using (var ipfs = new IpfsClient(new Uri("http://127.0.0.1:5001"), httpClient))
             {
-                Stream outputStream = await ipfs.Cat(Hash);
+                try
+                {
+                    Stream outputStream = await ipfs.Cat(Hash);
+                    FileStream writeStream = new FileStream(_path + "\\file", FileMode.Create, FileAccess.Write);
+                    ReadWriteStream(outputStream, writeStream);
 
-                //using (StreamReader sr = new StreamReader(outputStream))
-                //{
-                //    string contents = sr.ReadToEnd();
-                //    Console.WriteLine(contents); //Contents of test.txt are printed here!
-                //}
-
-                // create a write stream
-                FileStream writeStream = new FileStream(_path + "\\file", FileMode.Create, FileAccess.Write);
-                // write to the stream
-                ReadWriteStream(outputStream, writeStream);
-
-                Process.Start(_path + "\\file");
-
+                    Process.Start(_path + "\\file");
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+             
             }               
         }
 
@@ -144,7 +123,6 @@ namespace TrusChain.Storage
             int Length = 256;
             Byte[] buffer = new Byte[Length];
             int bytesRead = readStream.Read(buffer, 0, Length);
-            // write the required bytes
             while (bytesRead > 0)
             {
                 writeStream.Write(buffer, 0, bytesRead);
